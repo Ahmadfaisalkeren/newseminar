2a45c8667b9edea3a8f8e5090a6a7c1ffb51061d
Ahmad Faisal <maulidae.rizki@gmail.com>
2024-08-04 09:54:08 +0700

    removing frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,18 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LoroVel</title>
+    <title>Backend Seminar</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>
-
-    <div id="root"></div>
-
-    @viteReactRefresh
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js"
-        data-client-key="{{ config('services.midtrans.clientKey') }}"></script>
+    <h1>There's Nothing Here Dude</h1>
 </body>
 
 </html>
